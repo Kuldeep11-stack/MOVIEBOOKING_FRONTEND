8ffc93df279dec268a65823885ad9ee0bcd5991b
Kuldeep <kuldeepmer11@gmail.com>
2023-10-20 02:07:01 +0530

    home_page_dev

---
+++ src/components/CelebCard/CelebCard.tsx
@@ -0,0 +1,15 @@
+import { CelebrityCardType } from '@/types/types'
+import React from 'react'
+import Image from 'next/image'
+import './CelebCard.css';
+const CelebCard = (data:CelebrityCardType) => {
+  return (
+    <div className='celebcard'>
+        <Image src = {data.imageUrl} alt={data.name} width={200} height={200}/>
+        <h3>{data.name}</h3>
+        <h3>{data.role}</h3>
+    </div>
+  )
+}
+
+export default CelebCard
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from '@/assests/logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import './Navbar.css'
+import {BiUserCircle,BiSearch} from 'react-icons/bi'
+import {RiArrowDropDownFill} from 'react-icons/ri'
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="left">
+            <Image src={logo} alt="logo" width={100} height={100} />
+            <div className="searchbox">
+                <BiSearch className="searchbtn"/>
+                <input type ="text"placeholder='Search for a movie'/>
+            </div>
+        </div>
+        <div className="right">
+          <p className='dropdown' >Mumbai <RiArrowDropDownFill className="dropicon"/> </p>
+          <Link href="/" className='theme_btn1 linkstylenone'>Logout</Link>
+          <Link href="/" className='linkstylenone'>
+            <BiUserCircle className="theme_icon1/"/>
+          </Link>
+        </div>
+        </nav>
+  )
+}
+
+export default Navbar
